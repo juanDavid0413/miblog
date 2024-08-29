8d11a4fa419c4d9f26af9f0c55a6610731d00ee5
JuanDavid0413 <ju99da04ca13va@gmail.com>
2024-08-28 23:06:32 -0500

    Se crea la vista para mostrar los detalles de cada publicacion al hacer click sobre ellas, se cre su template html y se configura su url

--- blog_juandavid/appblog/urls.py
+++ blog_juandavid/appblog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import lista_publicaciones, agregar_publicacion
+from .views import lista_publicaciones, agregar_publicacion, detalle_publicacion
 
 urlpatterns = [
     path('publicaciones/', lista_publicaciones, name='lista_publicaciones'),
     path('agregar_publicacion/', agregar_publicacion, name='agregar_publicacion'),
+    path('detalle/<pk>/', detalle_publicacion, name='detalle_publicacion'),
 ]
--- blog_juandavid/appblog/views.py
+++ blog_juandavid/appblog/views.py
@@ -8,18 +8,19 @@ from django.core.paginator import Paginator
 def lista_publicaciones(request):
     query = request.GET.get('q')
     publicacion_list = Publicacion.objects.all()
-    paginator = Paginator(publicacion_list, 10)
     fecha_min = request.GET.get('fecha_min')
     fecha_max = request.GET.get('fecha_max')
-    page_number = request.GET.get('page')
-    publicaciones = paginator.get_page(page_number)
-
+   
     if fecha_min and fecha_max:
-        publicacion_list = publicacion_list.filter(fecha_publicacion__gte=fecha_min, fecha_publicacion__lte=fecha_max)
+        publicacion_list = publicacion_list.filter(fecha_publicacion_gte=fecha_min, fecha_publicacion_lte=fecha_max)
 
     if query:
         publicacion_list = publicacion_list.filter(Q(titulo__icontains=query) | Q(contenido__icontains=query))        
-   
+
+    paginator = Paginator(publicacion_list, 10)
+    page_number = request.GET.get('page')
+    publicaciones = paginator.get_page(page_number)
+
     return render(request, 'lista_publicaciones.html', {'publicaciones': publicaciones})
 
 
@@ -36,3 +37,7 @@ def agregar_publicacion(request):
     else:
         form = PublicacionModelForm()
     return render(request, 'agregar_publicacion.html', { 'form': form })
+
+def detalle_publicacion(request, pk):
+    publicacion = Publicacion.objects.get(pk=pk)
+    return render(request, 'detalle_publicacion.html', {'publicacion': publicacion})
